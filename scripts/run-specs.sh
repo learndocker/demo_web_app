e70e6d71b8b1e80d4be250bef195ee4d0e403c5f
Julian Fahrer <julian@fahrer.net>
2018-02-24 13:06:20 -0800

    Extract a build artifact via docker cp [refs #f2c17c9ea522]

--- scripts/run-specs.sh
+++ scripts/run-specs.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-docker-compose run app bundle exec rspec
+set -e
+function copy_artifacts {
+  mkdir -p /tmp/artifacts
+  CONTAINER_ID=$(docker-compose ps -q app)
+  docker cp ${CONTAINER_ID}:/tmp/rspec.xml /tmp/artifacts/rspec.yml
+}
+
+docker-compose up -d app
+docker-compose exec app \
+  bundle exec rspec --format progress --format RspecJunitFormatter --out /tmp/rspec.xml
+STATUS=$?
+copy_artifacts
+exit ${STATUS}
